9d37c411e40b460ffef90e1a77eba0488b24fa33
JustinWong98 <justinwong8991@gmail.com>
2021-12-09 23:33:24 +0800

    added minting plus more on buying process functionality

---
+++ contracts/Token.sol
@@ -0,0 +1,77 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+// import base ERC-721 functionality
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+// increment IDs when minting
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract SantaToken is ERC721URIStorage {
+    using Counters for Counters.Counter;
+
+    Counters.Counter public _tokenIds;
+    NFTForSale[] public _listed;
+    struct NFTForSale {
+        uint256 id;
+        uint256 price;
+        bool isSold;
+        // may not need this since we have it in wishlist already as a mapping
+        address owner;
+        // gifter should just be one of our accounts until it is actually gifted
+        address gifter;
+    }
+
+    constructor() ERC721URIStorage("SantaToken", "SNTA") {}
+
+    // custodian is our account
+    function mintNFT(
+        string memory _tokenURI,
+        uint256 _price,
+        address _custodian
+    ) {
+        uint256 newItemId = _tokenIds.current();
+
+        // in case we want them to be able to mint their own nfts as a stretch goal
+        _safeMint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        // custodian can transfer
+        approve(custodian, newItemId);
+
+        NFTForSale memory token = NFTForSale(
+            newItemID,
+            price,
+            false,
+            _custodian,
+            _custodian
+        );
+        _listed.push(token);
+        _tokenIds.increment();
+    }
+
+    // display price of NFT
+    function displayPrice(uint256 _itemId) external view returns (uint256) {
+        NFTForSale storage nftOnSale = _listed[_itemId];
+        return nftOnSale._price;
+    }
+
+    // called by wishlist - custodian account
+    function transferNFT(
+        address _currentOwner,
+        address _newOwner,
+        uint256 _itemId
+    ) external {
+        // use _currentOwner?
+        safeTransferFrom(_listed[_itemId].owner, _newOwner, _listed[_itemId]);
+    }
+
+    function markAsSold(uint256 _itemId) internal {
+        NFTForSale storage nft = _listed[_itemId];
+        nft.isSold = true;
+    }
+
+    function setGifter(uint256 _itemId, address _gifter) internal {
+        NFTForSale storage nft = _listed[_itemId];
+        nft.gifter = _gifter;
+    }
+}
--- contracts/Wishlist.sol
+++ contracts/Wishlist.sol
@@ -1,11 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
+import "./Token.sol";
+
 contract Wishlist {
     // map to wish
     mapping(uint256 => address) public wishes;
     // map inapp currency
     mapping(address => uint256) public points;
+
+    // list of NFTs
+
+    // import Tokens
+    SantaToken public token;
 
     // if user hasn't joined before
     function initUser(address _from) public {
@@ -18,8 +25,32 @@ contract Wishlist {
     }
 
     // fulfill a wish
-    function buyWish(address _from, uint256 _itemId) public {}
+    function buyWish(address _from, uint256 _itemId) public payable {
+        uint256 price = token.displayPrice(_itemId);
+        uint256 amountPaid = msg.value;
+        require(amountPaid >= price);
+        // smart contract owns all the NFTS (nft addresses point to smart contract address)
+        // smart contract takes the money
+        address payable currentOwner = payable(token.ownerOf(_itemId));
+        currentOwner.transfer(amountPaid);
+        // then smart contract releases the nft to the wishmaker
+        // may be using currentOwner as opposed to just the custodian so that we can implement trading inbetween users after they are sold?
+        token.transferNFT(currentOwner, wishes[_itemId], _itemId);
+        token.markAsSold(_itemId);
+        // we need to store the buyers address which is tied to the NFT now
+        token.setGifter(_itemId, _from);
+    }
 
     // redeem incentve
-    function redeem(address _from, uint256 _itemId) public {}
+    function redeem(address _from, uint256 _itemId) public {
+        // may need to change 0 to how much the NFT costs
+        require(points[_from] > 0);
+        points[_from] -= 1;
+        // incentive nft now points to _from
+    }
+
+    // view points
+    function getPoints(address _from) public view returns (uint256) {
+        return points[_from];
+    }
 }
